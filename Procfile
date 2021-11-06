9ebd622d6d5ec4e3954fea750fadac1980ec1de5
EDavies2020 <emz201080@yahoo.co.uk>
2021-11-06 11:14:11 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ladeez_golf.wsgi:application
--- ladeez_golf/settings.py
+++ ladeez_golf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*5o44niu9y6kzq^92a*kgd4u(&-(6c7+&l5_t--sph+pd&k1#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['edavies2020-ladeez-golf.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,19 @@ WSGI_APPLICATION = 'ladeez_golf.wsgi.application'
 # # Database
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
